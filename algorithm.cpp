ffb1e813a1a0cbbfaf6c22de3c4808e5d3a31307
marek357 <marekmmasiak@gmail.com>
2020-03-16 19:29:30 +0100

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -7,14 +7,10 @@
 #include <string>
 #include <unistd.h>
 
-
-//6, 12, 30, 102, 300, 600, 900
-
 #define MATSIZE 999
 #define PROC_NUM 3
 
 using namespace std;
-
 
 int A[3000][3000];
 int B[3000][3000];
@@ -22,12 +18,8 @@ int C[3000][3000];
 int c_arr[MATSIZE*MATSIZE];
 int C_fin[MATSIZE][MATSIZE];
 
-
-
-
 void loadC() {
     std::fstream cfile("csmall.txt", std::ios_base::in);
-
     int c_temp;
     int c_rows = 0;
     int c_counter = 0;
@@ -39,8 +31,6 @@ void loadC() {
             c_counter = 0;
         }
     }
-
-
     cfile.close();
 }
 
@@ -78,34 +68,22 @@ int getxyz(int x, int y, int z) {
 double start, stop;
 int division;
 int main(int argc, char *argv[]) {
-
     int numer_procesu;
     int ile_procesow;
     int ranks[] = {0};
-
     if (MATSIZE % PROC_NUM == 0) {
         division = MATSIZE / PROC_NUM;
     } else {
         division = 1;
     }
-
-
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&numer_procesu);
     MPI_Comm_size(MPI_COMM_WORLD,&ile_procesow);
-
-
     int result = 0;
     int result_fin = 0;
-
-
         if (numer_procesu == 0) {
-
             std::fstream afile("asmall.txt", std::ios_base::in);
             std::fstream bfile("bsmall.txt", std::ios_base::in);
-
-
-
             int a;
             int counter = 0;
             int rows = 0;
@@ -117,10 +95,8 @@ int main(int argc, char *argv[]) {
                     counter = 0;
                 }
             }
-
             counter = 0;
             rows = 0;
-
             while (bfile >> a and rows < MATSIZE) {
                 B[rows][counter] = a;
                 counter++;
@@ -129,8 +105,6 @@ int main(int argc, char *argv[]) {
                     counter = 0;
                 }
             }
-
-
             afile.close();
             bfile.close();
 	    start = MPI_Wtime();
@@ -138,20 +112,16 @@ int main(int argc, char *argv[]) {
             int counter_col = 0;
             int starting_point = 1;
             int initial_starting_point = 1;
-
             int counter_row_b = 0;
             int counter_col_b = 0;
             int starting_point_b = 1;
             int initial_starting_point_b = 1;
-
-
             int ranks_to_send[(PROC_NUM*PROC_NUM)+1];
             int ranks_to_send_b[(PROC_NUM*PROC_NUM)+1];
             int counter_row_num = 0;
             int counter_row_num_b = 0;
             int array_counter = 1;
             int array_counter_b = 1;
-
             for (int z = 1; z <= PROC_NUM*PROC_NUM*PROC_NUM; z++) {
                 ranks_to_send[array_counter+((z-1)%PROC_NUM)*PROC_NUM] = z;
                 if (z % PROC_NUM == 0) {
@@ -160,7 +130,6 @@ int main(int argc, char *argv[]) {
                     array_counter++;
                 }
             }
-
             for (int z = 1; z <= PROC_NUM*PROC_NUM*PROC_NUM; z += PROC_NUM) {
                 ranks_to_send_b[array_counter_b] = z;
                 array_counter_b++;
@@ -168,9 +137,6 @@ int main(int argc, char *argv[]) {
                     z++;
                 }
             }
-            //printA();
-            // printf("\n");
-             printB();
             //sendA
             for (int i = 1; i <= PROC_NUM*PROC_NUM; i++) {
                 int temp_starting_point = starting_point;
@@ -191,7 +157,6 @@ int main(int argc, char *argv[]) {
                     starting_point += division;
                 }
             }
-
             //SendB
             for (int i = 1; i <= PROC_NUM*PROC_NUM; i++) {
                 int temp_starting_point_b = starting_point_b;
@@ -212,11 +177,7 @@ int main(int argc, char *argv[]) {
                     starting_point_b += division;
                 }
             }
-
-
-
         } else {
-
             int buffora[division*division];
             int bufforb[division*division];
             MPI_Status status;
@@ -236,11 +197,9 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
-
             //Recieve B
             MPI_Recv(bufforb, division*division, MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
             MPI_Recv(&type_b, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
             if (type_b == 1) {
                 int offset = (numer_procesu-1)/(PROC_NUM*PROC_NUM);
                 int leftoffset = ((PROC_NUM-1)%PROC_NUM)+1;
@@ -253,12 +212,8 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
-
-
             int shift_right = ((numer_procesu-1)%PROC_NUM*PROC_NUM)%PROC_NUM;
             int shift_bottom = ((numer_procesu-1)%PROC_NUM*PROC_NUM)/PROC_NUM;
-
-            // printf("Numer procesu: %d\n", numer_procesu);
             int output[MATSIZE*MATSIZE];
             for (int i = 0; i < division; i++) {
                 for (int j = 0; j < division; j++) {
@@ -267,60 +222,17 @@ int main(int argc, char *argv[]) {
                         // sum += A[i][k]*B[k][j];
                         sum += buffora[i*division+k] * bufforb[k*division+j];
                     }
-                    // C[i+((((numer_procesu-1)%(PROC_NUM*PROC_NUM))/PROC_NUM)*division)][j+((((numer_procesu-1)%(PROC_NUM*PROC_NUM))%PROC_NUM)*division)] = sum;
-
                     C[i+((((numer_procesu-1)%(PROC_NUM*PROC_NUM))%PROC_NUM)*division)][j+((((numer_procesu-1)%(PROC_NUM*PROC_NUM))/PROC_NUM)*division)] = sum;
-
-
-
-
-
-
-
-                    // output[(i+(((numer_procesu%(PROC_NUM*PROC_NUM))/PROC_NUM)*PROC_NUM)*division)+j+(((numer_procesu%(PROC_NUM*PROC_NUM))%PROC_NUM)*PROC_NUM)] = sum;
-                    // C[i+shift_bottom*division][j+shift_right*division] = sum;
-
-                    // printf("%d %d counter: %d\n", i+(numer_procesu%(PROC_NUM*PROC_NUM))/PROC_NUM, j+(numer_procesu%(PROC_NUM*PROC_NUM))%PROC_NUM,counter);
-
                 }
-
             }
-
-
-
-
-
         }
-
         for (int i = 0; i < MATSIZE; i++) {
             MPI_Reduce(C[i], C_fin[i], MATSIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
         }
 
         if (numer_procesu == 0) {
             stop = MPI_Wtime();
-//             printA();
-   //          printf("-------\n");
-        //     printB();
-       //      printf("-------\n");
-             /*for (int i = 0; i < MATSIZE; i++) {
-                 for (int j = 0; j < MATSIZE; j++) {
-                    printf("%d ", C_fin[i][j]);
-                 }
-                 printf("\n");
-             }*/
-
-//             printf("\n");
- //           printf("%f\n", stop-start);
         }
-
-
-    // MPI_Reduce(&c_arr, &C_fin, MATSIZE*MATSIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    // if (numer_procesu == 0) {
-    //     stop = MPI_Wtime();
-    //     printf("%f\n", stop-start);
-    // }
-
-
     MPI_Finalize();
 
     return 0;
